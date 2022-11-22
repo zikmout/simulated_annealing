52fae888d844cf95802f85e7b1335a3bb2847c4b
Simon Sicard <sicsim64@gmail.com>
2022-11-22 12:02:33 +0100

    implementation

---
+++ State.js
@@ -0,0 +1,13 @@
+class State {
+  constructor(coords, score) {
+    this.coords = coords;
+    this.score = score;
+  }
+}
+
+class StateSpace_2D {
+  constructor() {
+    this.scores = [];
+    this.maxScore = 0;
+  }
+}
